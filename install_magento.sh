bbacab545963a9033416e96c74cbf602f5694558
Maciej Nalewczynski <maciej.nalewczynski@pwc.com>
2024-10-16 17:45:39 +0200

    add possibility for sample content installation

--- install_magento.sh
+++ install_magento.sh
@@ -6,12 +6,7 @@ if [ ! -d /var/www/html/magento2 ]; then
   composer config --global $MAGENTO_REPO $API_KEY $API_SECRET
   composer create-project --repository-url=https://repo.magento.com/ $MAGENTO_VERSION $magento_path
 
-  #official Adobe instructions
   cd $magento_path
-  find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
-  find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
-  chown -R :www-data . # Ubuntu
-  chmod u+x bin/magento
 
   php $magento_path/bin/magento setup:install \
     --db-host docker-magento2_db_1 --db-name magento2 --db-user magento2 --db-password magento2 --admin-user admin --timezone $TIMEZONE --currency $CURRENCY --use-rewrites 1 --cleanup-database \
@@ -21,6 +16,7 @@ if [ ! -d /var/www/html/magento2 ]; then
     --page-cache=redis --page-cache-redis-server=cache --page-cache-redis-port=6379 --page-cache-redis-db=1 \
     --search-engine=elasticsearch7 --elasticsearch-host=elasticsearch
 
+  cd $magento_path
   #disable 2factor auth
   php bin/magento module:disable Magento_AdminAdobeImsTwoFactorAuth
   php bin/magento module:disable Magento_TwoFactorAuth
@@ -30,6 +26,28 @@ if [ ! -d /var/www/html/magento2 ]; then
   php bin/magento dev:source-theme:deploy
   php bin/magento setup:static-content:deploy -f
 
+  #deploy sample content
+  if [ "$MAGENTO_SAMPLE_CONTENT" = "yes" ]; then
+    echo "Starting sample content installation.."
+    cd $magento_path
+    composer config $MAGENTO_REPO $API_KEY $API_SECRET
+    php bin/magento sampledata:deploy
+    php bin/magento setup:upgrade
+    php bin/magento setup:di:compile
+    php bin/magento cache:clean
+  else
+    echo "Skipping sample content installation.."
+  fi
+
+  cd $magento_path
+  #official Adobe instructions
+  find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
+  find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
+  chown -R :www-data . # Ubuntu
+  chmod u+x bin/magento
+  chmod 644 /var/www/html/magento2/auth.json
+
+  echo "Magento deployment finished!"
 else
   echo "Magento already installed, skipping installation.."
 fi
