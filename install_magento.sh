c9419d8fec53c1273ad772a86f44c654451b51bd
Maciej Nalewczynski <maciej.nalewczynski@pwc.com>
2024-10-17 10:45:11 +0200

    allowing custom naming of projects

--- install_magento.sh
+++ install_magento.sh
@@ -9,7 +9,7 @@ if [ ! -d /var/www/html/magento2 ]; then
   cd $magento_path
 
   php $magento_path/bin/magento setup:install \
-    --db-host docker-magento2_db_1 --db-name magento2 --db-user magento2 --db-password magento2 --admin-user admin --timezone $TIMEZONE --currency $CURRENCY --use-rewrites 1 --cleanup-database \
+    --db-host $COMPOSE_PROJECT_NAME_db_1 --db-name magento2 --db-user magento2 --db-password magento2 --admin-user admin --timezone $TIMEZONE --currency $CURRENCY --use-rewrites 1 --cleanup-database \
     --backend-frontname admin --admin-firstname AdminFirstName --admin-lastname AdminLastName --admin-email $ADMIN_EMAIL --admin-password $ADMIN_PASSWORD --base-url 'https://magento2.docker/' --language en_US \
     --session-save=redis --session-save-redis-host=sessions --session-save-redis-port=6379 --session-save-redis-db=0 --session-save-redis-password='' \
     --cache-backend=redis --cache-backend-redis-server=cache --cache-backend-redis-port=6379 --cache-backend-redis-db=0 \
